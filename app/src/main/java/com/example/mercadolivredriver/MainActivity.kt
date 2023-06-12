691c991872750ec8c4923d513095fc5a39d5d461
Leandro-PC <leandro-pitstop@hotmail.com>
2023-06-12 20:10:48 -0300

    include RecyclerView 12/06/2023

--- app/src/main/java/com/example/mercadolivredriver/MainActivity.kt
+++ app/src/main/java/com/example/mercadolivredriver/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.mercadolivredriver
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: MyAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dataSet = listOf("Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7",
+            "Item 8", "Item 9", "Item 10", "Item 11", "Item 12", "Item 13", "Item 14",)
+
+        recyclerView = findViewById(R.id.recyclerView)
+        adapter = MyAdapter(dataSet)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
